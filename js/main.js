6dfd1de75343253264bb6d47dcfae5b90911824d
stefantsova <stefantsovam@gmail.com>
2021-10-06 20:44:01 +0700

    Вносит исправления в функцию генерации случайного целого числа из диапазона

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-// Источник первой функции: MDN
+// Источник первой функции: MDN и помощь наставника :)
 
 function getRandomIntInclusive(min, max) {
+  if (min > max) {
+    const oldMin = min;
+    min = max;
+    max = oldMin;
+  }
   if (min >= 0 && max >=0) {
     min = Math.ceil(min);
     max = Math.floor(max);
