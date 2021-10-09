cdf8db3b21bd2257f82be9fa18255b5d701cae83
stefantsova <stefantsovam@gmail.com>
2021-10-09 16:53:33 +0700

    Вносит исправления во вспомогательные функции

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 // Источник первой функции: MDN и помощь наставника :)
 
-function getRandomIntInclusive(min, max) {
-  if (min > max) {
-    const oldMin = min;
-    min = max;
-    max = oldMin;
-  }
+const getRandomIntInclusive = (min, max) => {
   if (min >= 0 && max >=0) {
+    if (min > max) {
+      const oldMin = min;
+      min = max;
+      max = oldMin;
+    }
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-}
+};
 
-function checkCommentLength(comment, maxLength) {
-  return comment.length <= maxLength;
-}
+const checkCommentLength = (comment, maxLength) => comment.length <= maxLength;
 
 getRandomIntInclusive(2,10);
 checkCommentLength('Привет', 140);
